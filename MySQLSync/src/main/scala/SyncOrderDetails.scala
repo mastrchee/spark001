fb1a4d293f6b0a71e1c523a9176c4ed772c603e9
Rasika Jay <rasika.jayatillake@secretsales.com>
2015-12-10 12:24:09 +0000

    use sku

--- MySQLSync/src/main/scala/SyncOrderDetails.scala
+++ MySQLSync/src/main/scala/SyncOrderDetails.scala
@@ -23,14 +23,15 @@ object SyncOrderDetails {
     val awsSecret = System.getenv("AWS_SECRET_ACCESS_KEY")
 
     val table = "order_details"
-    val tableKey = "order_detail_id"
+    val tableUniqueKey = "order_detail_id"
+    val tableLastUpdatedKey = "last_updated"
     val S3Path = "secretsales-analytics/RedShift/Load/"+table+"/"+(System.currentTimeMillis / 1000)
 
     val tmp = sqlContext.read.format("jdbc").options(Map(
         ("url", redshiftHost),
         ("user", redshiftUser),
         ("password", redshiftPassword),
-        ("dbtable","(select max("+tableKey+") as max_id from "+table+") tmp"),
+        ("dbtable","(select max("+tableUniqueKey+") as max_id from "+table+") tmp"),
         ("driver", "com.amazon.redshift.jdbc41.Driver")
       )).load()
 
@@ -47,17 +48,18 @@ object SyncOrderDetails {
     // get data
     val data = new JdbcRDD(sparkContext,
       () => DriverManager.getConnection(mysqlHost, mysqlUser, mysqlPassword),
-      "SELECT od.order_detail_id, o.order_id, od.product_id, IFNULL(p.collection_id, 0) as 'collection_id', o.total_price, od.price, IFNULL(p.store_price, 0) as 'cost_price', o.discount, o.vat, o.vat_value, o.added FROM order_details od INNER JOIN orders o ON od.order_id = o.order_id LEFT JOIN products p ON od.product_id = p.id WHERE od.order_detail_id > "+maxId+" LIMIT ?, ?",
+      "SELECT od.order_detail_id, o.order_id, pc.sku, IFNULL(p.collection_id, 0) as 'collection_id', o.total_price, od.price, IFNULL(pc.cost_price, 0) as 'cost_price', o.discount, o.vat, o.vat_value, o.added FROM order_details od INNER JOIN orders o ON o.order_id = od.order_id INNER JOIN products p ON p.id = od.product_id INNER JOIN product_options po ON (od.product_id =  po.product_id AND od.option_id = po.id) INNER JOIN product_collection pc ON pc.collection_id = p.collection_id AND pc.sku = po.sku WHERE od.order_detail_id > "+maxId+" LIMIT ?, ?",
       0, 10000, 100, r => Row(
         r.getInt("order_detail_id"),
         r.getInt("order_id"),
-        r.getInt("product_id"),
+        r.getString("sku"),
         r.getInt("collection_id"),
         r.getFloat("cost_price"),
         r.getFloat("price"),
         if (r.getFloat("discount") > 0) (r.getFloat("price")/r.getFloat("total_price")).toFloat*r.getFloat("discount") else 0.0f, // disount per product (ternary check because of s**tty data)
         if (r.getFloat("vat") > 0) (r.getFloat("price")/(100.0f+r.getFloat("vat"))) * r.getFloat("vat") else 0.0f, // vat_value per product (ternary check because of s**tty data)
-        r.getTimestamp("added")
+        if (r.getString("added") == null) "" else r.getString("added"),
+        ""
       )
     )
 
@@ -65,13 +67,14 @@ object SyncOrderDetails {
     val schema = StructType(Array(
       StructField("order_detail_id",IntegerType,true),
       StructField("order_id",IntegerType,true),
-      StructField("product_id",IntegerType,true),
+      StructField("sku",StringType,true),
       StructField("collection_id",IntegerType,true),
       StructField("cost_price",FloatType,true),
       StructField("price",FloatType,true),
       StructField("discount",FloatType,true),
       StructField("vat_value",FloatType,true),
-      StructField("created",TimestampType,true)
+      StructField("created",StringType,true),
+      StructField("updated",StringType,true)
     ))
 
     // convert to DataFrame
@@ -79,6 +82,6 @@ object SyncOrderDetails {
 
     // copy to redshift
     val RedShift = new RedShift(redshiftHost, redshiftUser, redshiftPassword, awsKey, awsSecret)
-    RedShift.CopyFromDataFrame(DF, table, S3Path)
+    RedShift.CopyFromDataFrame(DF, table, S3Path, tableUniqueKey)
   }
 }
